1267f6f7415c93a2b7bf61512876bc3502ddb605
kcheung3102 <kcheung3102@gmail.com>
2019-11-11 16:52:59 -0500

    commit

--- src/SocialMediaApp.java
+++ src/SocialMediaApp.java
@@ -1,13 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class SocialMediaApp {
+    private static ArrayList<SocialMedia> socialMediaDB = new ArrayList<>();
 
     public static void main(String[] args) {
 	    boolean logIn = true;
 	    boolean toContinue = true;
 	    Scanner input = new Scanner(System.in);
+        SocialMedia newUser = new SocialMedia("john123", "password");
+        socialMediaDB.add(newUser);
 
     do {
         System.out.println("Do you want to use Facebook,Twitter, Instagram, or LinkedIn?");
@@ -17,64 +21,75 @@ public class SocialMediaApp {
             Facebook facebook = new Facebook();
             System.out.println(facebook.message());
             ArrayList<Facebook> facebookUsers = getfbUserLogin();
-            do {
-                for (Facebook f : facebookUsers) {
-                    System.out.println(f.displayText());
-                }
-                System.out.println("Do you want to log out?(Y/N)");
-                String str = input.nextLine().trim();
-                if (str.equalsIgnoreCase("y")) {
-                    logIn = false;
-                }
+            if(facebookUsers != null) {
+                do {
+                    for (Facebook f : facebookUsers) {
+                        System.out.println(f.displayText());
+                    }
+                    System.out.println("Do you want to log out?(Y/N)");
+                    String str = input.nextLine().trim();
+                    if (str.equalsIgnoreCase("y")) {
+                        logIn = false;
+                    }
 
-            } while (logIn);
-            System.out.println("You have logged out");
+                } while (logIn);
+
+                System.out.println("You have logged out");
+            }
         } else if (media.equalsIgnoreCase("twitter")) {
             Twitter twitter = new Twitter();
             System.out.println(twitter.message());
             ArrayList<Twitter> twitterUsers = getTwitUserLogin();
-            do {
-                for (Twitter t : twitterUsers) {
-                    System.out.println(t.displayText());
-                }
-                System.out.println("Do you want to log out?(Y/N)");
-                String str = input.nextLine().trim();
-                if (str.equalsIgnoreCase("y")) {
-                    logIn = false;
-                }
-            } while (logIn);
-            System.out.println("You have logged out");
+            if(twitterUsers != null) {
+                do {
+                    for (Twitter t : twitterUsers) {
+                        System.out.println(t.displayText());
+                    }
+                    System.out.println("Do you want to log out?(Y/N)");
+                    String str = input.nextLine().trim();
+                    if (str.equalsIgnoreCase("y")) {
+                        logIn = false;
+                    }
+                } while (logIn);
+                System.out.println("You have logged out");
+            }
         } else if (media.equalsIgnoreCase("linkedin")) {
             LinkedIn linkedin = new LinkedIn();
             System.out.println(linkedin.message());
             ArrayList<LinkedIn> linkedIn = getLinkUserLogin();
-            do {
-                for (LinkedIn l : linkedIn) {
-                    System.out.println(l.displayText());
-                }
-                System.out.println("Do you want to log out?(Y/N)");
-                String str = input.nextLine().trim();
-                if (str.equalsIgnoreCase("y")) {
-                    logIn = false;
-                }
+            if (linkedIn != null) {
+                do {
+                    for (LinkedIn l : linkedIn) {
+                        System.out.println(l.displayText());
+                    }
+                    System.out.println("Do you want to log out?(Y/N)");
+                    String str = input.nextLine().trim();
+                    if (str.equalsIgnoreCase("y")) {
+                        logIn = false;
+                    }
 
-            } while (logIn);
+                } while (logIn);
+            }
             System.out.println("You have logged out");
-        } else if (media.equalsIgnoreCase("instagram")) {
+        }
+        else if (media.equalsIgnoreCase("instagram")) {
             Instagram insta = new Instagram();
             System.out.println(insta.message());
+
             ArrayList<Instagram> instagram = getInstaLogin();
-            do {
-                for (Instagram l : instagram) {
-                    System.out.println(l.displayText());
-                }
-                System.out.println("Do you want to log out?(Y/N)");
-                String str = input.nextLine().trim();
-                if (str.equalsIgnoreCase("y")) {
-                    logIn = false;
-                }
-            } while (logIn);
-            System.out.println("you have logged out");
+            if (instagram != null) {
+                do {
+                    for (Instagram l : instagram) {
+                        System.out.println(l.displayText());
+                    }
+                    System.out.println("Do you want to log out?(Y/N)");
+                    String str = input.nextLine().trim();
+                    if (str.equalsIgnoreCase("y")) {
+                        logIn = false;
+                    }
+                } while (logIn);
+                System.out.println("you have logged out");
+            }
         } else {
             System.out.println("not valid media selection");
         }
@@ -99,9 +114,15 @@ public class SocialMediaApp {
         System.out.println("Enter password: ");
         password = input.nextLine();
 
-        //store the input data into arraylist
-        fbUser.add(new Facebook(userName, password));
-        return fbUser;
+        boolean validation = toValid(userName, password);
+        if(validation) {
+            //store the input data into arraylist
+            fbUser.add(new Facebook(userName, password));
+            return fbUser;
+        } else {
+            System.out.println("Invalid");
+            return null;
+        }
     }
 
     private static ArrayList<Twitter> getTwitUserLogin(){
@@ -113,10 +134,15 @@ public class SocialMediaApp {
         userName = input.nextLine();
         System.out.println("Enter password: ");
         password = input.nextLine();
-
-        //store the input data into arraylist
-        twitUser.add(new Twitter(userName, password));
-        return twitUser;
+        boolean validation = toValid(userName, password);
+        if(validation) {
+            //store the input data into arraylist
+            twitUser.add(new Twitter(userName, password));
+            return twitUser;
+        } else {
+            System.out.println("Invalid");
+            return null;
+        }
     }
 
     private static ArrayList<LinkedIn> getLinkUserLogin(){
@@ -128,10 +154,15 @@ public class SocialMediaApp {
         userName = input.nextLine();
         System.out.println("Enter password: ");
         password = input.nextLine();
-
+        boolean validation = toValid(userName, password);
         //store the input data into arraylist
-        linkedInUser.add(new LinkedIn(userName, password));
-        return linkedInUser;
+        if(validation) {
+            linkedInUser.add(new LinkedIn(userName, password));
+            return linkedInUser;
+        } else {
+            System.out.println("Invalid");
+            return null;
+        }
     }
 
     private static ArrayList<Instagram> getInstaLogin(){
@@ -144,9 +175,25 @@ public class SocialMediaApp {
         System.out.println("Enter password: ");
         password = input.nextLine();
 
-        //store the input data into arraylist
-       instaUser.add(new Instagram(userName, password));
-        return instaUser;
+        boolean validation = toValid(userName, password);
+        if(validation) {
+            //store the input data into arraylist
+            instaUser.add(new Instagram(userName, password));
+            return instaUser;
+        } else {
+            System.out.println("Invalid");
+            return null;
+        }
+
+    }
+
+    public static boolean toValid(String userName, String password){
+        for(SocialMedia sm: socialMediaDB ) {
+            if(userName.equalsIgnoreCase(sm.getUserName()) && password.equalsIgnoreCase(sm.getPassword())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
